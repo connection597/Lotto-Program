4baa0d565e9f389c53c71416822f10c7f4defa9d
connection579 <wlswn4118@naver.com>
2020-11-10 18:59:49 +0900

    로또 프로그램 연습

---
+++ src/lottoProgram/Lotto.java
@@ -0,0 +1,30 @@
+package lottoProgram;
+
+public class Lotto {
+
+	public static void main(String[] args) {
+		int lotto[] = new int[6];
+		
+		//번호 생성
+		for (int i =0; i<6; i++) {
+			lotto[i] = (int)(Math.random()*6)+1;
+			
+			//중복번호 제거
+			for(int j =0; j<i; j++) {
+				if(lotto[i] == lotto[j]) {
+					i--;
+					break;
+				}
+			}
+		}
+		System.out.print("로또번호 : ");
+		
+		//번호 출력
+		for(int i=0; i<6; i++) {
+			System.out.print(lotto[i] + "");
+			
+		}
+
+	}
+
+}
